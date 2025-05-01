2d4eb0feb3532437b1143da944e828cfc0cab719
hussam146 <hussamnasser38@gmail.com>
2025-05-01 19:24:32 +0300

    Fix issues #38

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/ready", function (req, res) {
 });
 
 app.listen(4000, "0.0.0.0", () => {
- console.log("Server successfully running on port - " + 4000);
+console.log("Server successfully running on port - " + 4000);
 });
 module.exports = app;
 
